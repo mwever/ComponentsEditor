6f9357fe9c77d6cb90d16f92cacea1784b883512
Helen Beierling <helebeen@mail.upb.de>
2019-02-27 12:02:26 +0100

    spring boot class

---
+++ ComponentEditor/src/main/java/Entity/ProvidedInterface.java
@@ -0,0 +1,22 @@
+package Entity;
+
+public class ProvidedInterface {
+	private String provInterface;
+
+	public String getProvInterface() {
+		return provInterface;
+	}
+
+	public void setProvInterface(String provInterface) {
+		this.provInterface = provInterface;
+	}
+
+	public ProvidedInterface(String provInterface) {
+		super();
+		this.provInterface = provInterface;
+	}
+	
+	public ProvidedInterface() {
+		
+	}
+}
