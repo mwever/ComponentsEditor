e8f9e0937ccbefb957d77c23f9d90becc8ad3a3d
Helen Beierling <helebeen@mail.upb.de>
2019-10-13 16:33:36 +0200

    finish parse method for Component to intermediate Component used by the Client

--- ComponentEditor/src/main/java/Controller/ComponentsController.java
+++ ComponentEditor/src/main/java/Controller/ComponentsController.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import Data.intermediate.BooleanParameterDomain;
 import Data.intermediate.CategoricalParameterDomain;
+import Data.intermediate.DefaultDomain;
 import Data.intermediate.Dependency;
 import Data.intermediate.IntermediateComponent;
 import Data.intermediate.Kitten;
@@ -27,6 +28,7 @@ import Data.intermediate.NumericParameterDomain;
 import Data.intermediate.Parameter;
 import Data.intermediate.ProvidedInterface;
 import Data.intermediate.RequiredInterface;
+import Data.intermediate.SelectionType;
 import Service.ComponentService;
 import hasco.model.Component;
 import hasco.model.ParameterDomain;
@@ -76,7 +78,7 @@ public class ComponentsController {
 	}
 
 	public static Component parseComponent(IntermediateComponent icomp) throws IOException {
-		
+
 		if (icomp.getName() == "") {
 			throw new IllegalArgumentException("Components must have a name");
 		}
@@ -100,11 +102,14 @@ public class ComponentsController {
 			List<Parameter> param = icomp.getParameters();
 			for (Parameter p : param) {
 				if (p.getDefaultDomain() instanceof NumericParameterDomain) {
-					Data.intermediate.NumericParameterDomain inp = (Data.intermediate.NumericParameterDomain) p.getDefaultDomain();
-					hasco.model.NumericParameterDomain np = new hasco.model.NumericParameterDomain(inp.isInteger(), inp.getMin(), inp.getMax());
+					Data.intermediate.NumericParameterDomain inp = (Data.intermediate.NumericParameterDomain) p
+							.getDefaultDomain();
+					hasco.model.NumericParameterDomain np = new hasco.model.NumericParameterDomain(inp.isInteger(),
+							inp.getMin(), inp.getMax());
 					comp.addParameter(new hasco.model.Parameter(p.getName(), np, inp.getDefaultValue()));
 				} else if (p.getDefaultDomain() instanceof CategoricalParameterDomain) {
-					Data.intermediate.CategoricalParameterDomain icp = (Data.intermediate.CategoricalParameterDomain) p.getDefaultDomain();
+					Data.intermediate.CategoricalParameterDomain icp = (Data.intermediate.CategoricalParameterDomain) p
+							.getDefaultDomain();
 					String[] values = new String[icp.getValues().size()];
 					int counter = 0;
 					for (Kitten k : icp.getValues()) {
@@ -114,7 +119,8 @@ public class ComponentsController {
 					hasco.model.CategoricalParameterDomain cp = new hasco.model.CategoricalParameterDomain(values);
 					comp.addParameter(new hasco.model.Parameter(p.getName(), cp, icp.getDefaultValue()));
 				} else if (p.getDefaultDomain() instanceof BooleanParameterDomain) {
-					Data.intermediate.BooleanParameterDomain ibp = (Data.intermediate.BooleanParameterDomain) p.getDefaultDomain();
+					Data.intermediate.BooleanParameterDomain ibp = (Data.intermediate.BooleanParameterDomain) p
+							.getDefaultDomain();
 					hasco.model.BooleanParameterDomain bp = new hasco.model.BooleanParameterDomain();
 					comp.addParameter(new hasco.model.Parameter(p.getName(), bp, ibp.getDefaultValue()));
 				}
@@ -157,7 +163,7 @@ public class ComponentsController {
 			System.out.println(preName);
 
 			if (inSplitted[1].contains("{")) {
-				String catString = inSplitted[1].substring(inSplitted[1].indexOf('{')+1, inSplitted[1].indexOf('}'));
+				String catString = inSplitted[1].substring(inSplitted[1].indexOf('{') + 1, inSplitted[1].indexOf('}'));
 				String[] catValues = catString.split(",");
 
 				if (catValues.length == 2) {
@@ -165,25 +171,27 @@ public class ComponentsController {
 						hasco.model.BooleanParameterDomain preDomain = new hasco.model.BooleanParameterDomain();
 						hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 						output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
-						
+
 					} else {
 						if (catValues[0].equalsIgnoreCase("false") && catValues[1].equalsIgnoreCase("true")) {
 							hasco.model.BooleanParameterDomain preDomain = new hasco.model.BooleanParameterDomain();
 							hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 							output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
-						}else {
-							hasco.model.CategoricalParameterDomain preDomain = new hasco.model.CategoricalParameterDomain(catValues);
+						} else {
+							hasco.model.CategoricalParameterDomain preDomain = new hasco.model.CategoricalParameterDomain(
+									catValues);
 							hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 							output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
 						}
 					}
-					
-				}else {
-					hasco.model.CategoricalParameterDomain preDomain = new hasco.model.CategoricalParameterDomain(catValues);
+
+				} else {
+					hasco.model.CategoricalParameterDomain preDomain = new hasco.model.CategoricalParameterDomain(
+							catValues);
 					hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 					output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
 				}
-				
+
 			} else if (inSplitted[1].contains("[")) {
 				String tmp = inSplitted[1].substring(inSplitted[1].indexOf('[') + 1, inSplitted[1].indexOf(']'));
 				String[] intervalValues = tmp.split(",");
@@ -192,12 +200,14 @@ public class ComponentsController {
 				boolean isInteger;
 				if (intervalValues[0].contains(".")) {
 					isInteger = false;
-					hasco.model.NumericParameterDomain preDomain = new hasco.model.NumericParameterDomain(isInteger, firstValue, secondValue);
+					hasco.model.NumericParameterDomain preDomain = new hasco.model.NumericParameterDomain(isInteger,
+							firstValue, secondValue);
 					hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 					output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
 				} else {
 					isInteger = true;
-					hasco.model.NumericParameterDomain preDomain = new hasco.model.NumericParameterDomain(isInteger, firstValue, secondValue);
+					hasco.model.NumericParameterDomain preDomain = new hasco.model.NumericParameterDomain(isInteger,
+							firstValue, secondValue);
 					hasco.model.Parameter preParam = new hasco.model.Parameter(preName, preDomain, null);
 					output.add(new Pair<hasco.model.Parameter, ParameterDomain>(preParam, preDomain));
 				}
@@ -206,51 +216,175 @@ public class ComponentsController {
 		System.out.println(output.toString());
 		return output;
 	}
-	
+
 	public IntermediateComponent reparseComponent(Component comp) {
-		//Name 
+		// Name
 		IntermediateComponent output = new IntermediateComponent(comp.getName());
-		
+
 		// Provided Interfaces
 		ArrayList<ProvidedInterface> provIList = new ArrayList<>();
-		for(String provI : comp.getProvidedInterfaces()) {
+		for (String provI : comp.getProvidedInterfaces()) {
 			ProvidedInterface intProvI = new ProvidedInterface(provI);
 			provIList.add(intProvI);
 		}
 		output.setProvidedInterfaces(provIList);
-		
-		//Required Interfaces
+
+		// Required Interfaces
 		ArrayList<RequiredInterface> reqIList = new ArrayList<>();
-		for(Entry<String, String> entry :comp.getRequiredInterfaces().entrySet()) {
+		for (Entry<String, String> entry : comp.getRequiredInterfaces().entrySet()) {
 			RequiredInterface intReqI = new RequiredInterface(entry.getKey(), entry.getValue());
 			reqIList.add(intReqI);
 		}
 		output.setRequiredInterfaces(reqIList);
-		
-		//Parameter
+
+		// Parameter
 		ArrayList<Parameter> paramList = new ArrayList<>();
-		for(hasco.model.Parameter param:comp.getParameters().getLinearization()) {
-			Parameter intParam = new Parameter(param.getName(), , null, null)
+		for (hasco.model.Parameter param : comp.getParameters().getLinearization()) {
+			String type = "";
+			DefaultDomain defaultdom = null;
+			if (param.getDefaultDomain() instanceof hasco.model.NumericParameterDomain) {
+				type = "number";
+				defaultdom = new NumericParameterDomain(
+						((hasco.model.NumericParameterDomain) param.getDefaultDomain()).getMin(),
+						((hasco.model.NumericParameterDomain) param.getDefaultDomain()).getMax(),
+						((hasco.model.NumericParameterDomain) param.getDefaultDomain()).isInteger(), type,
+						(double) param.getDefaultValue());
+			} else {
+				if (param.getDefaultDomain() instanceof hasco.model.BooleanParameterDomain) {
+					type = "bool";
+					defaultdom = new BooleanParameterDomain(new String[] { "true", "false" }, type,
+							(String) param.getDefaultValue());
+				} else {
+					type = "cat";
+
+					
+					ArrayList<Kitten> kit = new ArrayList<Kitten>();
+					for (String str : ((hasco.model.CategoricalParameterDomain) param.getDefaultDomain()).getValues()) {
+						kit.add(new Kitten(str));
+					}
+					defaultdom = new CategoricalParameterDomain(kit, type, ((String) param.getDefaultValue()));
+				}
+			}
+			paramList.add(new Parameter(param.getName(), type, defaultdom,
+					new SelectionType[] { new SelectionType("Cat", new CategoricalParameterDomain(null, "", "")),
+							new SelectionType("Number", new NumericParameterDomain(0, 0, false, "number", 0)),
+							new SelectionType("Bool", new BooleanParameterDomain(null, "", "")) }));
 		}
-		
-		
-		//Dependency
+
+		// Dependency
 		ArrayList<Dependency> depList = new ArrayList<>();
-		for(hasco.model.Dependency dep: comp.getDependencies()) {
-			Pair<String,String> prePost = parseDependencyToStringPair(dep);
-			Dependency intDep = new Dependency(prePost.getX(),prePost.getY());
+		for (hasco.model.Dependency dep : comp.getDependencies()) {
+			Pair<String, String> prePost = parseDependencyToStringPair(dep);
+			Dependency intDep = new Dependency(prePost.getX(), prePost.getY());
 			depList.add(intDep);
 		}
 		output.setDependencies(depList);
-		
-		
-		
+
 		return output;
-		
+
 	}
-	
-	private Pair<String,String> parseDependencyToStringPair(hasco.model.Dependency dep){
-		return null;
+
+	private Pair<String, String> parseDependencyToStringPair(hasco.model.Dependency dep) {
 		
+		// Premise parse
+		String pre = "";
+
+		int counter = 0;
+
+		for (Collection<Pair<hasco.model.Parameter, ParameterDomain>> pairs : dep.getPremise()) {
+			int end = pairs.size();
+			for (Pair<hasco.model.Parameter, ParameterDomain> d : pairs) {
+				if (d.getY() instanceof hasco.model.NumericParameterDomain) {
+					if (counter < end) {
+						pre += d.getX().getName() + " in " + "["
+								+ ((hasco.model.NumericParameterDomain) d.getY()).getMin() + ","
+								+ ((hasco.model.NumericParameterDomain) d.getY()).getMax() + "]";
+						pre += "and";
+					} else {
+						pre += d.getX().getName() + " in " + "["
+								+ ((hasco.model.NumericParameterDomain) d.getY()).getMin() + ","
+								+ ((hasco.model.NumericParameterDomain) d.getY()).getMax() + "]";
+					}
+				} else {
+					if (d.getY() instanceof hasco.model.CategoricalParameterDomain) {
+						if (d.getY() instanceof hasco.model.BooleanParameterDomain) {
+							if (counter < end) {
+								pre += d.getX().getName() + " in " + "{true,false}";
+								pre += "and";
+							} else {
+								pre += d.getX().getName() + " in " + "{true,false}";
+							}
+						} else {
+							if (counter < end) {
+								pre += d.getX().getName() + " in " + "{";
+								int counterValue = 0;
+								int endValue = ((hasco.model.CategoricalParameterDomain) d.getY()).getValues().length;
+								for (String str : ((hasco.model.CategoricalParameterDomain) d.getY()).getValues()) {
+									if (counterValue < endValue) {
+										pre += str + ",";
+									} else {
+										pre += str;
+									}
+								}
+								pre = "}";
+							}
+						}
+					}
+				}
+
+				counter++;
+			}
+		}
+		
+		//Conclusion parse
+		
+		String post = "";
+		counter = 0;
+		int end = dep.getConclusion().size();
+
+		for (Pair<hasco.model.Parameter, ParameterDomain> pairs : dep.getConclusion()) {
+			if (pairs.getY() instanceof hasco.model.NumericParameterDomain) {
+				if (counter < end) {
+					post += pairs.getX().getName() + " in " + "[" + ((hasco.model.NumericParameterDomain) pairs.getY()).getMin()
+							+ "," + ((hasco.model.NumericParameterDomain) pairs.getY()).getMax() + "]";
+					post += "and";
+				} else {
+					post += pairs.getX().getName() + " in " + "[" + ((hasco.model.NumericParameterDomain) pairs.getY()).getMin()
+							+ "," + ((hasco.model.NumericParameterDomain) pairs.getY()).getMax() + "]";
+				}
+			} else {
+				if (pairs.getY() instanceof hasco.model.CategoricalParameterDomain) {
+					if (pairs.getY() instanceof hasco.model.BooleanParameterDomain) {
+						if (counter < end) {
+							post += pairs.getX().getName() + " in " + "{true,false}";
+							post += "and";
+						} else {
+							post += pairs.getX().getName() + " in " + "{true,false}";
+						}
+					} else {
+						if (counter < end) {
+							post += pairs.getX().getName() + " in " + "{";
+							int counterValue = 0;
+							int endValue = ((hasco.model.CategoricalParameterDomain) pairs.getY()).getValues().length;
+							for (String str : ((hasco.model.CategoricalParameterDomain) pairs.getY()).getValues()) {
+								if (counterValue < endValue) {
+									post += str + ",";
+								} else {
+									post += str;
+								}
+							}
+							post = "}";
+						}
+					}
+				}
+
+				counter++;
+			}
+		}
+
+		
+
+		return new Pair<>(pre, post);
+
 	}
 }
